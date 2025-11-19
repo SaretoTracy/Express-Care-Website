f08c6e6d15c7e61dac71245e640bb1da0c96dd5a
Sareto <tracysareto@gmail.com>
2025-11-19 16:52:12 +0300

    change the IproviderSignup to match the providersignup API Json format

--- src/Interfaces/IProviderSignUp.ts
+++ src/Interfaces/IProviderSignUp.ts
@@ -1,20 +1,17 @@
-export interface IHomRepresentative {
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
-  }
-  export interface IHomeInfo {
-    homeName: string;
-    website?: string;
-    workEmail: string;
-    phoneNumber: string;
-    state: string;
-    city: string;
-    street: string;
-    zipcode: string;
-    terms: string;
-    adultHomeDescription: string;
-  }
-  
+export interface IProviderSignup {
+  first_name: string;         
+  last_name: string;          
+  email: string;            
+  password: string;            
+  confirmPassword: string;     
+  phone_number: string;       
+  job_title: string;          
+  adult_home_name: string;     
+  adult_home_email: string;    
+  adult_home_phone: string;    
+  adult_home_city: string;    
+  adult_home_state: string;    
+  adult_home_street: string;   
+  adult_home_zipcode: string;  
+  adult_home_website?: string; 
+}
